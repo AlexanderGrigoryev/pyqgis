3bd68f8db9ed8ebc2e1c5e065ed2eadfb0fff867
Alexander Grigoryev <30285620+AlexanderGrigoryev@users.noreply.github.com>
2020-03-16 11:43:07 +0700

    dynamic dialog and plugin with menu

--- agg_button/plugin.py
+++ agg_button/plugin.py
@@ -1,7 +1,7 @@
 import os.path
 from _lib.base_plugin import BasePlugin
+from _lib.base_dialog import BaseDialog
 from _lib.consts import Messages as m
-from .dialog import Dialog
 from .resources import *
 
 
@@ -13,7 +13,8 @@ class AggButton(BasePlugin):
                          icon=u'soska.png',
                          tool=u'Вызов диалога по кнопке',
                          tools=None,
-                         dialog=Dialog)
+                         dialog=BaseDialog,
+                         ui_file='dialog.ui')
 
     def run(self):
         self._dlg.show()
